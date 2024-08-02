84b1088b66c0e02edafaabc1b52454006b4ade72
dstupnikov <stupnikovdaniil@gmail.com>
2024-08-03 01:59:17 +0300

    Base data

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ import json
 import secrets
 from openpyxl import load_workbook
 
+
 # Настройки логирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
